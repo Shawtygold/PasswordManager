d125ebdcc7e16cdf7563c231f24eb2401e657818
shawtygold <shawtygoldz@gmail.com>
2024-06-24 18:15:34 +0500

    add: User validator and attributes for user entity

--- PasswordBox/MVVM/Model/Entities/User.cs
+++ PasswordBox/MVVM/Model/Entities/User.cs
@@ -1,8 +1,13 @@
-﻿namespace PasswordBox.MVVM.Model.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PasswordBox.MVVM.Model.Entities
 {
     internal class User
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required, MaxLength(50)]
         public string Login { get; set; }
 
         public User(string login) : this(0, login) { }
---
+++ PasswordBox/MVVM/Model/Validators/UserValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using PasswordBox.MVVM.Model.Entities;
+
+namespace PasswordBox.MVVM.Model.Validators
+{
+    internal class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.Id).NotNull().GreaterThan(-1);
+            RuleFor(u => u.Login).NotNull().MinimumLength(2).MaximumLength(50);
+        }
+    }
+}
