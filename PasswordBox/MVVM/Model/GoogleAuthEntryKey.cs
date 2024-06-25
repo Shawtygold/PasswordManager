ecfc2b713dcb2a049bc890aaf3e51b7c3df99ac5
shawtygold <mylnikovmisha@gmail.com>
2024-06-25 14:10:05 +0500

    Add a GoogleAuthEntryKey class to get the manual code for Google Authentificator

---
+++ PasswordBox/MVVM/Model/GoogleAuthEntryKey.cs
@@ -0,0 +1,19 @@
+﻿using Google.Authenticator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordBox.MVVM.Model
+{
+    internal class GoogleAuthEntryKey
+    {
+        public string GetEntryKey(string issuer, string accountLogin, string accountSecretKey)
+        {
+            TwoFactorAuthenticator twoFactorAuthenticator = new(HashType.SHA256);
+            SetupCode setupInfo = twoFactorAuthenticator.GenerateSetupCode(issuer, accountLogin, accountSecretKey, false, 3);
+            return setupInfo.ManualEntryKey;
+        }
+    }
+}
