9ab32e8ecc94765d4d4e031596851d13414212df
shawtygold <mylnikovmisha@gmail.com>
2024-06-25 13:37:31 +0500

    Add UserLoginValidator class
    This class will check if the user whose login is entered in the "Login"
    field exists when trying to log in to the application.

---
+++ PasswordBox/MVVM/Model/Validators/UserLoginValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using PasswordBox.MVVM.Model.Entities;
+using PasswordBox.MVVM.Model.Repositories;
+
+namespace PasswordBox.MVVM.Model.Validators
+{
+    internal class UserLoginValidator : UserValidator
+    {
+        public UserLoginValidator() : base()
+        {
+            RuleFor(u => u.Login).CustomAsync(async (login, context, cancellationToken) =>
+            {
+                User? user = null;
+                using (UserRepository userRepository = new(new AppContext.ApplicationContext()))
+                {
+                    try
+                    {
+                        user = await userRepository.GetByAsync(u => u.Login == login);
+                    }
+                    catch (Exception e)
+                    {
+                        dynamic ex = e;
+                        ExceptionsLogger.Handle(ex);
+                        context.AddFailure("An error occurred during user verification.");
+                        return;
+                    }
+                }
+
+                if (user == null)
+                {
+                    context.AddFailure("User with this login does not exist.");
+                    return;
+                }
+            });
+        }
+    }
+}
