44add5a6a174b4688e031acc3c4a888e98c282d0
shawtygold <mylnikovmisha@gmail.com>
2024-06-25 14:11:29 +0500

    Add a GoogleAuthValidator class to validate a user's two-factor code.

---
+++ PasswordBox/MVVM/Model/GoogleAuthValidator.cs
@@ -0,0 +1,13 @@
+﻿using Google.Authenticator;
+
+namespace PasswordBox.MVVM.Model
+{
+    internal class GoogleAuthValidator
+    {
+        public bool Validate(string login, string twoFactorCode)
+        {
+            TwoFactorAuthenticator twoFactorAuthenticator = new(HashType.SHA256);
+            return twoFactorAuthenticator.ValidateTwoFactorPIN(login, twoFactorCode);
+        }
+    }
+}
