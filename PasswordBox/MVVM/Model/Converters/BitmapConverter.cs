30331414c5548162d4095d81d61d145151c797e7
shawtygold <mylnikovmisha@gmail.com>
2024-06-30 13:06:13 +0500

    Add converters for interaction with Qr code image

---
+++ PasswordBox/MVVM/Model/Converters/BitmapConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace PasswordBox.MVVM.Model.Converters
+{
+    internal class BitmapConverter
+    {
+        internal static BitmapSource ConvertBitmapToBitmapSource(Bitmap bitmap)
+        {
+            var bitmapData = bitmap.LockBits(
+                new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                System.Drawing.Imaging.ImageLockMode.ReadOnly, bitmap.PixelFormat);
+
+            var bitmapSource = BitmapSource.Create(
+                bitmapData.Width, bitmapData.Height,
+                bitmap.HorizontalResolution, bitmap.VerticalResolution,
+                PixelFormats.BlackWhite, null,
+                bitmapData.Scan0, bitmapData.Stride * bitmapData.Height, bitmapData.Stride);
+
+            bitmap.UnlockBits(bitmapData);
+
+            return bitmapSource;
+        }
+    }
+}
---
+++ PasswordBox/MVVM/Model/Converters/QrCodeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace PasswordBox.MVVM.Model.Converters
+{
+    internal class QrCodeConverter
+    {
+        internal static BitmapSource ConvertQrCodeUrlToBitmapSource(string qrCodeUrl)
+        {
+            Bitmap qrBitmap = ConvertQrUrlToBitmap(qrCodeUrl);
+            return BitmapConverter.ConvertBitmapToBitmapSource(qrBitmap);
+        }
+        private static Bitmap ConvertQrUrlToBitmap(string qrUrl)
+        {
+            using (MemoryStream ms = new(Convert.FromBase64String(qrUrl.Replace("data:image/png;base64,", ""))))
+            {
+                return (Bitmap)Image.FromStream(ms);
+            }
+        }
+    }
+}
