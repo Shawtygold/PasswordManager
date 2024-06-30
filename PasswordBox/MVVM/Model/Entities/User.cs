c90294c6031406cf67cfa29839b59bd011853410
shawtygold <mylnikovmisha@gmail.com>
2024-06-30 12:06:40 +0500

    Add a constructor in User.cs for convenient validation in LoginVM

--- PasswordBox/MVVM/Model/Entities/User.cs
+++ PasswordBox/MVVM/Model/Entities/User.cs
@@ -5,16 +5,23 @@ namespace PasswordBox.MVVM.Model.Entities
 {
     internal class User
     {
+        public User(string login, string password) : this(0, login, password, "secretKey") { }
+        public User(string login, string password, string secretKey) : this(0, login, password, secretKey) { }
+        public User(int id, string login, string password, string secretKey)
+        {
+            Id = id;
+            Login = login;
+            SecretKey = secretKey;
+        }
+
+
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Required, MaxLength(50)]
         public string Login { get; set; }
-
-        public User(string login) : this(0, login) { }
-        public User(int id, string login)
-        {
-            Id = id;
-            Login = login;
-        }
+        [Required, MinLength(4)]
+        public string Password { get; set; }
+        [Required]
+        public string SecretKey { get; set; }
     }
 }
