953d8a1327c679c0989cfaacb032de2d480bb463
shawtygold <mylnikovmisha@gmail.com>
2024-06-30 13:02:12 +0500

    Add qr-code form and VM for it

---
+++ PasswordBox/MVVM/View/Forms/QrCodeForm.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace PasswordBox.MVVM.ViewModel.FormsViewModel
+{
+    /// <summary>
+    /// Логика взаимодействия для QrCodeForm.xaml
+    /// </summary>
+    public partial class QrCodeForm : Window
+    {
+        public QrCodeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
---
+++ PasswordBox/MVVM/ViewModel/FormsViewModel/QrCodeFormViewModel.cs
@@ -0,0 +1,59 @@
+﻿using PasswordBox.Core;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace PasswordBox.MVVM.ViewModel.FormsViewModel
+{
+    internal class QrCodeFormViewModel : Core.ViewModel
+    {
+        public QrCodeFormViewModel(ImageSource qrCode, string manualEntryKey)
+        {
+            QrCode = qrCode;
+            ManualKey = manualEntryKey;
+
+            CloseCommand = new RelayCommand(Close);
+            MinimizeCommand = new RelayCommand(Minimize);
+        }
+
+        #region Properties
+
+        private string _manualKey;
+        public string ManualKey
+        {
+            get { return _manualKey; }
+            set { _manualKey = value; OnPropertyChanged(); }
+        }
+
+        private ImageSource _qrCode;
+        public ImageSource QrCode
+        {
+            get { return _qrCode; }
+            set { _qrCode = value; OnPropertyChanged(); }
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand CloseCommand { get; set; }
+        public ICommand MinimizeCommand { get; set; }
+
+        private void Close(object obj)
+        {
+            if (obj is not Window form)
+                return;
+
+            form.Close();
+        }
+        private void Minimize(object obj)
+        {
+            if (obj is not Window form)
+                return;
+
+            form.WindowState = WindowState.Minimized;
+        }
+
+        #endregion
+    }
+}
