be8253679809fdc1ba23e24283dda49d60c48c2d
shawtygold <mylnikovmisha@gmail.com>
2024-06-30 13:13:35 +0500

    Delete unnecessary method and file

--- PasswordBox/Interfaces/Google Auth/IGoogleAuthenticator.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PasswordBox.Interfaces.Google_Auth
-{
-    internal interface IGoogleAuthenticator
-    {
-        string GetManualEntryKey(string login);
-        bool Validate(string login, string twoFactorCode);
-    }
-}
--- PasswordBox/MVVM/Model/DataWorker.cs
+++ PasswordBox/MVVM/Model/DataWorker.cs
@@ -4,24 +4,6 @@ namespace PasswordBox.MVVM.Model
 {
     class DataWorker
     {
-        //получение сообщения о том, как прошло действие(добавление/редактирование/удаление)  Например: "Пароль успешно добавлен!"
-        //internal static string GetMessageAboutActionWithThePassword(bool wasTheAction, string msgTrue, string msgFalse)
-        //{
-        //    string message;
-
-        //    если действие было совершено успешно
-        //    if (wasTheAction == true)
-        //    {
-        //        message = msgTrue;
-        //    }
-        //    else
-        //    {
-        //        message = msgFalse;
-        //    }
-
-        //    return message;
-        //}
-
         //метод получающий изображение
         internal static string GetImage()
         {
