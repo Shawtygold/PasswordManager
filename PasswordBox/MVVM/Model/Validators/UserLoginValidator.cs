124e793effb5b174089f69569dad0d259306b64b
shawtygold <mylnikovmisha@gmail.com>
2024-06-30 12:56:35 +0500

    Add a password check in UserLoginValidator.cs

--- PasswordBox/MVVM/Model/Validators/UserLoginValidator.cs
+++ PasswordBox/MVVM/Model/Validators/UserLoginValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using PasswordBox.MVVM.Model.AppContext;
 using PasswordBox.MVVM.Model.Entities;
 using PasswordBox.MVVM.Model.Repositories;
 
@@ -10,25 +11,34 @@ namespace PasswordBox.MVVM.Model.Validators
         {
             RuleFor(u => u.Login).CustomAsync(async (login, context, cancellationToken) =>
             {
-                User? user = null;
-                using (UserRepository userRepository = new(new AppContext.ApplicationContext()))
+                if (string.IsNullOrEmpty(login))
+                    return;
+
+                User? dbUser = null;
+                using (UserRepository userRepository = new(new ApplicationContext()))
                 {
                     try
                     {
-                        user = await userRepository.GetByAsync(u => u.Login == login);
+                        dbUser = await userRepository.GetByAsync(u => u.Login == login);
                     }
                     catch (Exception e)
                     {
                         dynamic ex = e;
                         ExceptionsLogger.Handle(ex);
-                        context.AddFailure("An error occurred during user verification.");
+                        context.AddFailure("An error occurred during user verification. Please contact with support.");
                         return;
                     }
                 }
 
-                if (user == null)
+                if (dbUser == null)
                 {
                     context.AddFailure("User with this login does not exist.");
+                    return;
+                }
+
+                if (context.InstanceToValidate.Password != dbUser.Password)
+                {
+                    context.AddFailure("Incorrect password.");
                     return;
                 }
             });
