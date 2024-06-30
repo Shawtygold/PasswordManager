2962b43c1a468cac9c39e0d23706249837995cc9
shawtygold <mylnikovmisha@gmail.com>
2024-06-30 15:04:49 +0500

    Add validation to the user constructor

--- PasswordBox/MVVM/Model/Entities/User.cs
+++ PasswordBox/MVVM/Model/Entities/User.cs
@@ -1,19 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using FluentValidation;
+using PasswordBox.MVVM.Model.Validators;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PasswordBox.MVVM.Model.Entities
 {
     internal class User
     {
-        public User(string login, string password) : this(0, login, password, "secretKey") { }
+        private readonly UserValidator _userValidator;
+
         public User(string login, string password, string secretKey) : this(0, login, password, secretKey) { }
         public User(int id, string login, string password, string secretKey)
         {
             Id = id;
             Login = login;
+            Password = password;
             SecretKey = secretKey;
-        }
 
+            _userValidator = new UserValidator();
+            _userValidator.ValidateAndThrow(this);
+        }
 
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
