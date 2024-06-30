cef0d5038049c56d1a244aa4537d46fa82d72ede
shawtygold <mylnikovmisha@gmail.com>
2024-06-30 13:09:56 +0500

    Add ToastNotificationService

---
+++ PasswordBox/MVVM/Model/ToastNotificationService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using PasswordBox.Interfaces.Notifications;
+
+namespace PasswordBox.MVVM.Model
+{
+    internal class ToastNotificationService : INotificationService
+    {
+        public void Send(ToastContentBuilder notification)
+        {
+            notification.Show();
+        }
+    }
+}
